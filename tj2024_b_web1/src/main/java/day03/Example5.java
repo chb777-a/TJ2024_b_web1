fa6b9135760513e0313ab16124e1c525083996a3
chb777-a <chb_b@naver.cm>
2025-01-20 15:38:01 +0900

    2025_01_21_setContentType_geWrite

---
+++ tj2024_b_web1/src/main/java/day03/Example5.java
@@ -0,0 +1,45 @@
+package day03;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/day03/example5")
+public class Example5 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		boolean result = true;						//1. 응답할 자료준비
+		resp.setContentType("application/json");	//2. HTTP를 이용한 응답 헤더 정보 추가 , .setContentType();
+		resp.getWriter().print(result);				//3. HTTP를 이용한 요청에 따른 응답 자료 보내기
+	} // get end
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String result = "java";
+		resp.setContentType("text/plain");			// 문자열 1개는 application/json 으로 타입변환 불가능 => text/plain 
+		resp.getWriter().print(result);
+	} // post end
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int result = 70;
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
+	} // put end
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		DataDto result = new DataDto("유재석", 40);	// 오류 : DTO를 JSON으로 타입변환 불가능. 
+		//DTO => JSON으로 변환
+		ObjectMapper mapper = new ObjectMapper();				// 1. ObjectMapper 인스턴스 생성
+		String jsonResult = mapper.writeValueAsString(mapper);	// 2. .writeValueAsString(변환할객체) : 지정한 객체를 JSON형식으로 변환 함수.
+		resp.setContentType("application/json");
+		resp.getWriter().print(jsonResult);
+	} // delete end
+}
